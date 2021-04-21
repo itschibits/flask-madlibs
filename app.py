be07e31b60849005a4819cae40cd855f65c3392e
S T <itschibits@gmail.com>
2021-04-21 19:35:44 -0400

    created dynamic form based on story

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
+from random import choice
 
-from stories import silly_story
+from stories import silly_story, excited_story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+
+STORIES = [silly_story, excited_story]
+
+
+@app.route('/')
+def madlibs_form():
+    """Return homepage"""
+
+    return render_template(
+        "questions.html",
+        words=choice(STORIES).prompts,
+        )
+
